08407651e0abd3522e3f3bb88def63fe710a9c2f
Andrew Koidan <deathangel908@gmail.com>
2019-03-27 12:26:09 +0200

    Replaced test checks with sinon

--- test/index.ts
+++ test/index.ts
@@ -1,49 +1,68 @@
 'use strict';
 
-import {assert, expect} from 'chai';
+import {assert, expect, use} from 'chai';
 import {LoggerFactory, Logger, LogStrict, MockConsole, DoLog} from '../src';
+import * as sinonChai from 'sinon-chai';
+import * as sinon from 'sinon';
+import {SinonSandbox, SinonSpy} from 'sinon';
 
+use(sinonChai);
+/* tslint:disable:no-unused-expression */
 describe('test logger', () => {
-  it('should debug with simple text', (done) => {
+  let spy: SinonSpy<unknown[], void> | null;
+  beforeEach(() => {
+    spy = null;
+  });
+
+  afterEach(() => {
+    if (spy) {
+      spy.restore();
+    }
+  });
+
+  const mockConsole: MockConsole = new (class implements MockConsole {
+    debug(message?: unknown, ...optionalParams: unknown[]): void {
+      console.log(`calling debug message: ${message} params: ${optionalParams.join(',')}`);
+    }
+
+    error(message?: unknown, ...optionalParams: unknown[]): void {
+      console.log(`calling error message: ${message} params: ${optionalParams.join(',')}`);
+    }
+
+    log(message?: unknown, ...optionalParams: unknown[]): void {
+      console.log(`calling log message: ${message} params: ${optionalParams.join(',')}`);
+    }
+
+    warn(message?: unknown, ...optionalParams: unknown[]): void {
+      console.log(`calling warn message: ${message} params: ${optionalParams.join(',')}`);
+    }
+  })();
+  it('should debug with simple text', () => {
     const tag = 't1';
     const style = 'color: red';
     const text = 'Hello world!';
+    spy = sinon.spy(mockConsole, `debug`);
     const loggerFactory: LoggerFactory =
-        new LoggerFactory(LogStrict.LOG_RAISE_ERROR, {
-          debug(...args: unknown[]) {
-            assert.equal(args[0], `%c${tag}`);
-            assert.equal(args[1], style);
-            assert.equal(args[2], text);
-            expect(args).to.have.length(3);
-            done();
-          }
-        } as MockConsole);
+        new LoggerFactory(LogStrict.LOG_RAISE_ERROR, mockConsole);
     const logger: Logger = loggerFactory.getLogger(tag, style);
     logger.debug(text)();
+    expect(spy).to.have.been.calledOnceWithExactly(`%c${tag}`, style, text);
   });
-  it('should log with single param', (done) => {
+  it('should log with single param', () => {
     const tag = 't1';
     const style = 'color: red';
     const textBefore = 'Hello ';
     const textAfter = '. How\'s it going!';
     const text = `${textBefore}{}${textAfter}`;
     const p1 = {a: 3};
+    spy = sinon.spy(mockConsole, `error`);
     const loggerFactory: LoggerFactory =
-        new LoggerFactory(LogStrict.LOG_RAISE_ERROR, {
-          error(...args: unknown[]) {
-            assert.equal(args[0], `%c${tag}`);
-            assert.equal(args[1], style);
-            assert.equal(args[2], textBefore);
-            assert.equal(args[3], p1);
-            assert.equal(args[4], textAfter);
-            expect(args).to.have.length(5);
-            done();
-          }
-        } as MockConsole);
+        new LoggerFactory(LogStrict.LOG_RAISE_ERROR, mockConsole);
     const logger: Logger = loggerFactory.getLogger(tag, style);
     logger.error(text, p1)();
+    expect(spy).to.have.been.calledOnceWithExactly(`%c${tag}`, style, textBefore, p1, textAfter);
   });
-  it('should work with multiple params', (done) => {
+  it('should work with multiple params', () => {
     const tag = 't1';
     const style = 'color: red';
     const textBefore = 'Hello ';
@@ -52,193 +71,131 @@ describe('test logger', () => {
     const textAfter = 'going';
     const text = `${textBefore}{}${textMiddle}{}${textAfter}`;
     const p1 = 'Mike';
+    spy = sinon.spy(mockConsole, `warn`);
     const loggerFactory: LoggerFactory =
-        new LoggerFactory(LogStrict.LOG_RAISE_ERROR, {
-          warn(...args: unknown[]) {
-            assert.equal(args[0], `%c${tag}`);
-            assert.equal(args[1], style);
-            assert.equal(args[2], textBefore);
-            assert.equal(args[3], p1);
-            assert.equal(args[4], textMiddle);
-            assert.equal(args[5], p2);
-            assert.equal(args[6], textAfter);
-            expect(args).to.have.length(7);
-            assert.equal(
-                args.join(''), '%ct1color: redHello Mike. How\'s it going');
-            done();
-          }
-        } as MockConsole);
+        new LoggerFactory(LogStrict.LOG_RAISE_ERROR, mockConsole);
     const logger: Logger = loggerFactory.getLogger(tag, style);
     logger.warn(text, p1, p2)();
+    expect(spy).to.have.been.calledOnceWithExactly(`%c${tag}`, style, textBefore, p1, textMiddle, p2,  textAfter);
   });
-  it(`Should warn if params are missing and LOG_WITH_WARNINGS`, (done) => {
-    let called = false;
+  it(`Should warn if params are missing and LOG_WITH_WARNINGS`, () => {
+    spy = sinon.spy(mockConsole, `log`);
+    const spy2 = sinon.spy(mockConsole, `error`);
     const loggerFactory: LoggerFactory =
-        new LoggerFactory(LogStrict.LOG_WITH_WARNINGS, {
-          log(...args: unknown[]) {
-            assert.equal(args.join('.'), '%ct2.style.<<<.1.===.>>>');
-            assert.isTrue(called, 'Error should be called');
-            done();
-          },
-          error(...args: unknown[]) {
-            called = true;
-          },
-        } as MockConsole);
+        new LoggerFactory(LogStrict.LOG_WITH_WARNINGS, mockConsole);
     const logger: Logger = loggerFactory.getLogger('t2', 'style');
     logger.log('<<<{}==={}>>>', 1)();
+    
+    expect(spy).to.have.been.calledOnceWithExactly(`%ct2`, 'style', '<<<', 1, '===', '>>>');
+    expect(spy2).to.have.been.calledOnce;
+    spy2.restore();
   });
-  it('should raise error if params more and LOG_RAISE_ERROR', (done) => {
+  it('should raise error if params more and LOG_RAISE_ERROR', () => {
     const loggerFactory: LoggerFactory =
         new LoggerFactory(LogStrict.LOG_RAISE_ERROR);
     const logger: Logger = loggerFactory.getLogger('t2', 'style');
     expect(() => {
       logger.log('<<<{}', 1, 2);
     }).to.throw('MissMatch amount of arguments');
-    done();
   });
-  it(`Shouldn't warn if warn disabled`, (done) => {
-    let called = false;
-    const loggerFactory: LoggerFactory = new LoggerFactory(LogStrict.TRACE, {
-      error(...args: unknown[]) {
-        called = true;
-      },
-      warn(...args: unknown[]) {
-        called = true;
-      },
-      debug(...args: unknown[]) {
-        called = true;
-      },
-      log(...args: unknown[]) {
-
-      }
-    });
+  it(`Shouldn't warn if warn disabled`, () => {
+    const loggerFactory: LoggerFactory = new LoggerFactory(LogStrict.TRACE, mockConsole);
+    const spy1 = sinon.spy(mockConsole, 'error');
+    const spy2 = sinon.spy(mockConsole, 'warn');
+    const spy3 = sinon.spy(mockConsole, 'debug');
+    const spy4 = sinon.spy(mockConsole, 'log');
     const logger: Logger = loggerFactory.getLogger('t2', 'style');
     logger.log('<<<{}', 1, 2)();
-    assert.equal(called, false);
-    done();
+    sinon.assert.notCalled(spy1);
+    sinon.assert.notCalled(spy2);
+    sinon.assert.notCalled(spy3);
+    expect(spy4).to.have.been.calledOnce;
+    spy1.restore();
+    spy2.restore();
+    spy3.restore();
+    spy4.restore();
   });
-  it(`shouldn't log if logs are off`, (done) => {
-    let called = false;
+  it(`shouldn't log if logs are off`, () => {
     const loggerFactory: LoggerFactory =
-        new LoggerFactory(LogStrict.DISABLE_LOGS, {
-          error(...args: unknown[]) {
-            called = true;
-          },
-          warn(...args: unknown[]) {
-            called = true;
-          },
-          debug(...args: unknown[]) {
-            called = true;
-          },
-          log(...args: unknown[]) {
-            called = true;
-          }
-        });
-    loggerFactory.setLogWarnings(LogStrict.DISABLE_LOGS);
+        new LoggerFactory(LogStrict.DISABLE_LOGS, mockConsole);
+
+    const spy1 = sinon.spy(mockConsole, 'error');
+    const spy2 = sinon.spy(mockConsole, 'warn');
+    const spy3 = sinon.spy(mockConsole, 'debug');
+    const spy4 = sinon.spy(mockConsole, 'log');
     const logger: Logger = loggerFactory.getLogger('t2', 'style');
     logger.log('Hello!')();
     logger.trace('Hello!')();
     logger.warn('Hello!')();
     logger.error('Hello!')();
     logger.debug('Hello!')();
-    assert.equal(called, false);
-    done();
+    sinon.assert.notCalled(spy1);
+    sinon.assert.notCalled(spy2);
+    sinon.assert.notCalled(spy3);
+    sinon.assert.notCalled(spy4);
+    spy1.restore();
+    spy2.restore();
+    spy3.restore();
+    spy4.restore();
   });
-  it(`should trace if log level is trace`, (done) => {
-    let called = false;
-    const loggerFactory: LoggerFactory = new LoggerFactory(LogStrict.TRACE, {
-      debug(...args: unknown[]) {
-        called = true;
-      }
-    } as MockConsole);
+  it(`should trace if log level is trace`, () => {
+    const loggerFactory: LoggerFactory = new LoggerFactory(LogStrict.TRACE, mockConsole);
+    spy = sinon.spy(mockConsole, 'debug');
     loggerFactory.setLogWarnings(LogStrict.TRACE);
     const logger: Logger = loggerFactory.getLogger('t2', 'style');
     logger.trace('Hello!')();
-    assert.equal(called, true);
-    done();
+    expect(spy).to.have.been.calledOnce;
   });
 
-  it(`should debug if log level is trace`, (done) => {
-    let called = false;
-    const loggerFactory: LoggerFactory = new LoggerFactory(LogStrict.TRACE, {
-      debug(...args: unknown[]) {
-        called = true;
-      }
-    } as MockConsole);
+  it(`should debug if log level is trace`, () => {
+    const loggerFactory: LoggerFactory = new LoggerFactory(LogStrict.TRACE, mockConsole);
+    spy = sinon.spy(mockConsole, 'debug');
     loggerFactory.setLogWarnings(LogStrict.TRACE);
     const logger: Logger = loggerFactory.getLogger('t2', 'style');
     logger.debug('Hello!')();
-    assert.equal(called, true);
-    done();
+    expect(spy).to.have.been.calledOnce;
   });
 
-  it(`shouldn't trace if debug`, (done) => {
-    let called = false;
-    const loggerFactory: LoggerFactory = new LoggerFactory(LogStrict.DEBUG, {
-      error(...args: unknown[]) {
-        called = true;
-      },
-      warn(...args: unknown[]) {
-        called = true;
-      },
-      debug(...args: unknown[]) {
-        called = true;
-      },
-      log(...args: unknown[]) {
-        called = true;
-      }
-    });
-    loggerFactory.setLogWarnings(LogStrict.DEBUG);
+  it(`shouldn't trace if debug`, () => {
+    const loggerFactory: LoggerFactory =
+        new LoggerFactory(LogStrict.DEBUG, mockConsole);
+    const spy1 = sinon.spy(mockConsole, 'error');
+    const spy2 = sinon.spy(mockConsole, 'warn');
+    const spy3 = sinon.spy(mockConsole, 'debug');
+    const spy4 = sinon.spy(mockConsole, 'log');
     const logger: Logger = loggerFactory.getLogger('t2', 'style');
     logger.trace('Hello!')();
-    assert.equal(called, false);
-    done();
+    sinon.assert.notCalled(spy1);
+    sinon.assert.notCalled(spy2);
+    sinon.assert.notCalled(spy3);
+    sinon.assert.notCalled(spy4);
+    spy1.restore();
+    spy2.restore();
+    spy3.restore();
+    spy4.restore();
   });
-  it(`getLoggerColor should work`, (done) => {
-    let called = false;
+  it(`getLoggerColor should work`, () => {
+    spy = sinon.spy(mockConsole, 'log');
     const loggerFactory: LoggerFactory =
-        new LoggerFactory(LogStrict.LOG_RAISE_ERROR, {
-          log(...args: unknown[]) {
-            assert.equal(args[0], '%ctest');
-            assert.equal(
-                args[1],
-                'color: white; background-color: black; padding: 2px 6px; border-radius: 2px; font-size: 10px');
-            assert.equal(args[2], 'before ');
-            assert.equal(args[3], 1);
-            assert.equal(args[4], ' after');
-
-            called = true;
-          }
-        } as MockConsole);
-    /* tslint:disable:no-unused-expression */
+        new LoggerFactory(LogStrict.LOG_RAISE_ERROR, mockConsole);
+    
     const logger: Logger = loggerFactory.getLoggerColor('test', 'black');
     logger.log('before {} after', 1)();
-    expect(called).to.be.true;
-    done();
+    expect(spy).to.have.been.calledOnceWithExactly(`%ctest`, 'color: white; background-color: black; padding: 2px 6px; border-radius: 2px; font-size: 10px', 'before ', 1, ' after');
   });
-  it(`getSingleLoggerColor should work`, (done) => {
+  it(`getSingleLoggerColor should work`, () => {
     const loggerFactory: LoggerFactory =
         new LoggerFactory(LogStrict.LOG_RAISE_ERROR, null);
-    let called = false;
+
+    const cb = sinon.spy();
     const fnLogger: DoLog = loggerFactory.getSingleLoggerColor(
-        'test', 'black', (...args: unknown[]) => {
-          assert.equal(args[0], '%ctest');
-          assert.equal(
-              args[1],
-              'color: white; background-color: black; padding: 2px 6px; border-radius: 2px; font-size: 10px');
-          assert.equal(args[2], '');
-          assert.equal(args[3], 1);
-          assert.equal(args[4], 2);
-          called = true;
-        });
+        'test', 'black', cb);
     fnLogger('{} 2', 1)();
-    /* tslint:disable:no-unused-expression */
-    expect(called).to.be.true;
-    done();
+    expect(cb).to.have.been.calledOnceWithExactly(`%ctest`, 'color: white; background-color: black; padding: 2px 6px; border-radius: 2px; font-size: 10px', '', 1, ' 2');
   });
-  it(`Default log level should be set`, (done) => {
+  it(`Default log level should be set`, () => {
     const loggerFactory: LoggerFactory = new LoggerFactory();
-    expect(loggerFactory.getLogWarnings()).to.be.equal(LogStrict.LOG_WITH_WARNINGS);
-    done();
+    expect(loggerFactory.getLogWarnings())
+        .to.be.equal(LogStrict.LOG_WITH_WARNINGS);
   });
 });
